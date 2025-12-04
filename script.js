243b8aeeb2971f743626ea7340af2bd575a62e58
Ahmed Alabadla <eng.ahmedalabadla@gmail.com>
2025-12-04 13:48:15 +0200

    Add footer

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const copyright = document.getElementById("copyright");
+
 document.addEventListener("DOMContentLoaded", function () {
+  const year = new Date().getFullYear();
+  copyright.textContent = year;
+
   const toggle = document.querySelector(".nav-toggle");
   const nav = document.getElementById("main-nav");
   const navLinks = document.querySelectorAll(".nav-list a");
+
+  const barsIcon = document.getElementById("bars");
+  const closeIcon = document.getElementById("close");
 
   if (!toggle || !nav) return;
 
@@ -9,12 +17,25 @@ document.addEventListener("DOMContentLoaded", function () {
     e.stopPropagation();
     nav.classList.toggle("open");
     toggle.classList.toggle("open");
+
+    // Toggle icons
+    if (nav.classList.contains("open")) {
+      barsIcon.classList.add("hidden");
+      closeIcon.classList.remove("hidden");
+    } else {
+      barsIcon.classList.remove("hidden");
+      closeIcon.classList.add("hidden");
+    }
   });
 
   navLinks.forEach((link) =>
     link.addEventListener("click", () => {
       nav.classList.remove("open");
       toggle.classList.remove("open");
+
+      // Reset icons
+      barsIcon.classList.remove("hidden");
+      closeIcon.classList.add("hidden");
     })
   );
 
@@ -23,6 +44,10 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!nav.contains(e.target) && !toggle.contains(e.target)) {
       nav.classList.remove("open");
       toggle.classList.remove("open");
+
+      // Reset icons
+      barsIcon.classList.remove("hidden");
+      closeIcon.classList.add("hidden");
     }
   });
 
